bee5ba84d6a362f2cf2defa2cafb4624f946c6c8
AlveeMorshed <alvee2015@gmail.com>
2023-02-08 12:28:45 +0600

    first commit

---
+++ app/src/main/java/com/moinul/cricvee/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moinul.cricvee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
